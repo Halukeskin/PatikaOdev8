cd22e9394d0380237acfdc450bd60b5c7d13118c
Keskin6161 <76918333+Keskin6161@users.noreply.github.com>
2021-05-20 12:45:58 +0300

    Add files via upload

---
+++ hata-yonetimi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz.");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş Olduğunuz Sayı:" +sayi);
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata:" + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem Tamamlandı.");
+            }
+
+            try
+            {
+                //int a = int.Parse(null);
+                int a = int.Parse("test");
+            }
+            catch (FormatException ex)
+            {
+                
+                Console.WriteLine("Bir değer girdiniz:");
+                Console.WriteLine(ex);
+            }
+
+
+        }
+    }
+}
